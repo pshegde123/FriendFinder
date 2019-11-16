5eeb46051fcce27d913acf04057c0d90de5b1554
pradnya <pradnyahegde@gmail.com>
2019-11-15 21:36:48 -0500

    default friends data file

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendsArray = [{
+    name: "friend 1",
+    photo: "www.example.com",
+    scores: [
+        5,
+        1,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        3,
+        4
+    ]
+}, {
+    name: "friend 2",
+    photo: "www.example.com",
+    scores: [
+        4,
+        5,
+        2,
+        3,
+        3,
+        5,
+        1,
+        3,
+        3,
+        4
+    ]
+}
+];
+
+module.exports = friendsArray;
