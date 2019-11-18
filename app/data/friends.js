57008b2d2a8bec1c38cce1a7eb774387861c78ef
pradnya <pradnyahegde@gmail.com>
2019-11-18 17:34:16 -0500

    added object Timothy

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsArray = [{
-    name: "friend 1",
-    photo: "www.example.com",
+    name: "Jim",
+    photo: "https://previews.123rf.com/images/meysye/meysye1904/meysye190400004/121250282-male-silhouette-avatar-default-avatar-profile-picture-photo-placeholder.jpg",
     scores: [
         5,
         1,
@@ -14,8 +14,8 @@ var friendsArray = [{
         4
     ]
 }, {
-    name: "friend 2",
-    photo: "www.example.com",
+    name: "Maria",
+    photo: "https://cdn.dribbble.com/users/312581/screenshots/1676038/female-placeholder.png",
     scores: [
         4,
         5,
@@ -24,6 +24,22 @@ var friendsArray = [{
         3,
         5,
         1,
+        3,
+        3,
+        4
+    ]
+},
+{
+    name: "Timothy",
+    photo: "https://miro.medium.com/max/545/1*UNf6kxqv_ujIjKoMI0q97A.png",
+    scores: [
+        4,
+        5,
+        3,
+        3,
+        3,
+        5,
+        2,
         3,
         3,
         4
